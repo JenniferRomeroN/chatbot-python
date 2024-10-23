5547ccef3efcb3cb0bd1a7c51dd20fedf8490929
jenni <jennifer_romero3@hotmail.com>
2024-10-23 14:09:16 -0700

    add method "back"

--- navegation.py
+++ navegation.py
@@ -8,10 +8,19 @@ driver = webdriver.Chrome()
 driver.set_window_position(0,0)
 driver.set_window_size(1024, 720)
 
+#navegacion hacia la pagina de whatsapp
+driver.get(("https://web.whatsapp.com/"))
+time.sleep(15)
+
 #navegacion hacia el url
 #abre el navegador en la pagina principal
 driver.get("https://google.com")
 #imprime el titulo de la pagina actual, en este ejemplo "google"
 print(driver.title)
 #el timepo que se pausa cuando se abre el navegador
-time.sleep(25)
+time.sleep(15)
+
+#metodo BACK
+driver.back()
+print(driver.current_url)
+time.sleep(5)
