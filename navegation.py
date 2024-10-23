6392a24dbfb9195a287545d337e2b27ad9c107da
jenni <jennifer_romero3@hotmail.com>
2024-10-23 13:57:24 -0700

    first commit

---
+++ navegation.py
@@ -0,0 +1,17 @@
+#hacemos las importaciones
+#webdriver es de selenium
+from selenium import webdriver
+#time es para pausar la ejecucion
+import time
+
+driver = webdriver.Chrome()
+driver.set_window_position(0,0)
+driver.set_window_size(1024, 720)
+
+#navegacion hacia el url
+#abre el navegador en la pagina principal
+driver.get("https://google.com")
+#imprime el titulo de la pagina actual, en este ejemplo "google"
+print(driver.title)
+#el timepo que se pausa cuando se abre el navegador
+time.sleep(25)
